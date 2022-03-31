b502bcad02ccd4f0218cf67489e3ef0c80c1d78b
Barbara <barbara.branco.c@gmail.com>
2022-03-31 16:53:28 +0200

    commitBeforeRevertReactROMClientEXP

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('hello, picly pickus'+e)
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('hello' + ' ' + name);
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Poke me</button>
+            <button onClick={() => handleClickAgain('mario')}>Click me again</button>
         </div>
      );
 }
