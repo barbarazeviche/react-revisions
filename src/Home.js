b02b0154393dafab4c7d70c61e14ef780d2dd8b7
Barbara <barbara.branco.c@gmail.com>
2022-04-04 15:06:42 +0200

    outputting lists

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,45 @@
+import { useState } from 'react';
+
 const Home = () => {
 
     
-    const handleClick=(e) => {
+    /* const handleClick=(e) => {
         console.log("hello ninjas", e);
-    }
+    } */
 
-    const handleClickAgain = (name, e) => {
-        console.log("hello " + name, e.target);
+   /*  const [name, setName] = useState('mario')
+    const [age, setAge] = useState('1')
+
+   
+
+    const handleClick = () => {
+        setName('luigi');
+        setAge('33');
     }
+ */
+  /*   const handleClickAgain = (name, e) => {
+        console.log("hello " + name, e.target);
+    } */
+
+
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'loremhgffghjk....', author:'mario', id: 1},
+        { title: 'welll welll welll', body: 'loremhgffghjk....', author:'maria', id: 2},
+        { title: 'going loca in here', body: 'loremhgffghjk....', author:'mariana', id: 3}
+    ]);
 
     return ( 
         <div className="home">
-            <h2>HomePAge</h2>
+            {blogs.map((blog) => (
+             <div className='blog-preview' key={blog.id}>
+                 <h2>{blog.title}</h2>
+                 <p> Written by {blog.author}</p>
+             </div>   
+            ))}
+           {/*  <h2>HomePAge</h2>
+            <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={(e)=>handleClickAgain("mario", e)}>Click me again</button>
+            <button onClick={(e)=>handleClickAgain("mario", e)}>Click me again</button> */}
         </div>
      );
 }
