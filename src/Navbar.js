7f86f0d8fead26d533cf248550d9851914062b39
Barbara <barbara.branco.c@gmail.com>
2022-04-04 11:48:59 +0200

    course#7

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (  
+        <nav className="navbar">
+            <h1>This is the Nav</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
